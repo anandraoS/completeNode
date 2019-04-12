23b1183b0229c99b5b70777bb4af24ae9b05b34e
ananda rao sahukari <sahukari.anandarao@gmail.com>
2019-04-12 13:57:31 +0530

    dynamic content startup

--- js refresher/app.js
+++ js refresher/app.js
@@ -1,18 +1,21 @@
-const http = require('http');
-const bodyParser = require('body-parser');
-const express = require('express');
 const path = require('path');
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
 
-const rootDir = require('./util/path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin',adminRouter);
-app.use(shopRouter);
 
-app.use((req,res,next)=>{
-    res.sendfile(path.join(rootDir,'views','404.html'));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- js refresher/routes/shop.js
+++ js refresher/routes/shop.js
@@ -1,14 +1,13 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('in the middle ware');
-    res.sendFile(path.join(rootDir,'views','shop.html'));
-   // res.send('<h1>Welcome to world</h1>');
-    // res.send({"name":"anand","number":123});
-    // next();
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
