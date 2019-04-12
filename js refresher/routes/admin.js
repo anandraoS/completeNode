98149ef1bcabe374ec5bd3c3e6f62843809e963d
ananda rao sahukari <sahukari.anandarao@gmail.com>
2019-04-12 16:34:58 +0530

    pug completed fully

--- js refresher/routes/admin.js
+++ js refresher/routes/admin.js
@@ -6,15 +6,18 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const data = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product',{pageTitle:'addintItem',path:'/admin/add-product'});
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  data.push({title:req.body.title});
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.data = data;
--- js refresher/routes/shop.js
+++ js refresher/routes/shop.js
@@ -5,9 +5,11 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
-
+const admindata = require('./admin').data;
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(admindata); res.render('shop',{path:'/', prods:admindata, pageTitle:'Shop'});
+  // res.render(path.join(rootDir,'views','shop'))
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
