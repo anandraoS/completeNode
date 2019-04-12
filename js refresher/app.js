c91f154ff140f06264ab77d08d8dabd28cd42098
ananda rao sahukari <sahukari.anandarao@gmail.com>
2019-04-12 11:49:46 +0530

    body-parser introduced

--- js refresher/app.js
+++ js refresher/app.js
@@ -1,16 +1,30 @@
 const http = require('http');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button>addTitle</button></form>');
+    //res.send({ "name": "anand", "number": 123 });
+    // next();
+});
+app.use('/product', (req, res, next) => {
+    console.log(req.body.title);
+res.redirect('/');
+})
+app.use('/', (req, res, next) => {
     console.log('in the middle ware');
-    next();
+    res.send('<h1>Welcome to world</h1>');
+    // res.send({"name":"anand","number":123});
+    // next();
 });
 
 app.use((req, res, next) => {
     console.log('in Another middle ware');
-    res.send({"name":"anand","number":123});
+    res.send({ "name": "anand", "number": 123 });
 });
 
 const requestHandler = require('./routes');
