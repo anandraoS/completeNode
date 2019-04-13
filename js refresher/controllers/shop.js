e9c1a6dbe9017b5ae0f4b9a27ab949fad28bb2d0
ananda rao sahukari <sahukari.anandarao@gmail.com>
2019-04-13 15:55:39 +0530

    adding to cart and cart model is added and quatity where handled

--- js refresher/controllers/shop.js
+++ js refresher/controllers/shop.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product');
-
-
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Products',
+      pageTitle: 'All Products',
       path: '/products'
     });
   });
@@ -14,13 +13,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findProductById(prodId, product => {
+  Product.findById(prodId, product => {
     res.render('shop/product-detail', {
       product: product,
-      pageTitle: product.title + ' detail',
-      path: '/product-detail'
+      pageTitle: product.title,
+      path: '/products'
     });
-    console.log(product);
   });
 };
 
@@ -33,26 +31,32 @@ exports.getIndex = (req, res, next) => {
     });
   });
 };
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
 };
+
 exports.postCart = (req, res, next) => {
-  productID = req.body.productId;
- console.log(productID);
- res.redirect('/cart');
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
     pageTitle: 'Your Orders'
   });
 };
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pateTitle: 'Checkout'
+    pageTitle: 'Checkout'
   });
-}
+};
