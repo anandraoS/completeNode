8860bb0fbab2da0e72392a882dd9f1079f740d8d
ananda rao sahukari <sahukari.anandarao@gmail.com>
2019-04-20 00:29:46 +0530

    bug fixed

--- js refresher/app.js
+++ js refresher/app.js
@@ -53,8 +53,8 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 sequelize
-    .sync({ force: true })
-    //.sync()
+    //.sync({ force: true })
+    .sync()
     .then(result => {
         //console.log(result);
         return User.findByPk(1);
